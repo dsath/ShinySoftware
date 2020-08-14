4d68b2c63000feb0cd3e9a0a47746048b4af32bc
Devin Sather <devinsather@live.com>
2020-08-13 17:34:24 -0700

    Refactored code. Much more readable.

--- test.cpp
+++ test.cpp
@@ -9,53 +9,65 @@
 using namespace cv;
 using namespace std;
 
+int* getBGR(Mat *f, int sr, int sc, int bh); 
+void addBlackBox(Mat *f, int sr, int sc, int bh); 
+
 int main(int argv, char** argc)
 {
   const int startRow = 150;
   const int startCol = 150;
   const int boxHeight = 30;
-
   const int fps = 20;
+  int *bgr;
  
-
-
-  //Mat original = imread("assets/pic.jpg", IMREAD_COLOR);
-  //Mat modified = imread("assets/pic.jpg", IMREAD_COLOR);
   Mat frame;
-  VideoCapture vid(2);
-
+  namedWindow("boxOne", CV_WINDOW_AUTOSIZE);
+  VideoCapture vid(2); 
 
   while (vid.read(frame)) {
+    bgr = getBGR(&frame, startRow, startCol, boxHeight);
+    addBlackBox(&frame, startRow, startCol, boxHeight);
 
-    int boxSize = boxHeight * boxHeight; 
-    int red, green, blue;
+    cout << "blue: " << bgr[0] << std::endl;
+    cout << "green: " << bgr[1] << std::endl;
+    cout << "red: " << bgr[2] << std::endl;
 
-    for (int r = startRow; r < startRow + boxHeight; ++r) {
-      for(int c = startCol; c < startCol + boxHeight; ++c) {
-        //store rgb values of current frame
-        blue += frame.at<Vec3b>(r, c)[0]; 
-        green += frame.at<Vec3b>(r, c)[1]; 
-        red += frame.at<Vec3b>(r, c)[2];
-
-        //make box black
-        frame.at<Vec3b>(r, c)[0] = 0; 
-        frame.at<Vec3b>(r, c)[1] = 0; 
-        frame.at<Vec3b>(r, c)[2] = 0; 
-      }
-    }
-      
-
-    blue = blue/(boxSize);
-    red = red/(boxSize);
-    green = green/(boxSize);
-
-    cout << "blue: " << blue << std::endl;
-    cout << "green: " << green << std::endl;
-    cout << "red: " << red << std::endl;
-    imshow("frame", frame);
+    imshow("boxOne", frame);
     if(cvWaitKey(1000 / fps) >= 0)
       break;
   }
   return 1;
 }
+
+int* getBGR(Mat *f, int sr, int sc, int bh) {
+    int *vals = new int[3] {0};
+
+    for (int r = sr; r < sr + bh; ++r) {
+      for(int c = sc; c < sc + bh; ++c) {
+        //store rgb values of current frame
+        vals[0] += f->at<Vec3b>(r, c)[0]; 
+        vals[1] += f->at<Vec3b>(r, c)[1]; 
+        vals[2] += f->at<Vec3b>(r, c)[2];
+      }
+    }
+
+    vals[0] /= (bh * bh);
+    vals[1] /= (bh * bh);
+    vals[2] /= (bh * bh);
+    return vals;
+}
+
+void addBlackBox(Mat *f, int sr, int sc, int bh) {
+    for (int r = sr; r < sr + bh; ++r) {
+      for(int c = sc; c < sc + bh; ++c) {
+        f->at<Vec3b>(r, c)[0] = 0; 
+        f->at<Vec3b>(r, c)[1] = 0; 
+        f->at<Vec3b>(r, c)[2] = 0;
+      }
+    }
+}
+
+
+
+
 
